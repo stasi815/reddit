0e8363b0851f1b38e643ebcd2ec0502e5b4ad506
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-02-03 14:59:30 -0800

    renders template

---
+++ server.js
@@ -0,0 +1,14 @@
+// require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 3000
+const exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
