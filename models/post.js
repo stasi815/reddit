3889fcd67bb049c99f140d61a8ddbb4b4e45f63d
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-02-03 17:25:28 -0800

    Users can submit and save posts

---
+++ models/post.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: { type: String, required: true },
+  url: { type: String, required: true },
+  summary: { type: String, required: true }
+});
+
+
+// Post = mongoose.model("Post", PostSchema)
+
+module.exports = mongoose.model("Post", PostSchema);
--- server.js
+++ server.js
@@ -1,14 +1,43 @@
-// require('dotenv').config();
+// Set db
+require('./data/reddit-db');
+
+// Require Libraries
 const express = require('express');
+
+// App Setup
 const app = express();
-const port = 3000
+
+// Static Files
+app.use(express.static('public'));
+
+// Server port
+const port = 3000;
+
+// Middleware
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+// Requirements
+require('./controllers/posts.js')(app);
+
+// Routes
 app.get('/', (req, res) => {
     res.render('home')
 })
 
+app.get('/posts/new', (req, res) => {
+    res.render('posts-new')
+})
+
+// Start Server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
